f5fa387353b990a90f3aa24cda0212acf7265981
CSJLU <lucourage@gmail.com>
2023-07-25 15:55:45 -0400

    Switch to socketio initialization

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import join_room, leave_room, send, SocketIO
+
+# db = SQLAlchemy()
+# DB_NAME = "database.db"
+
+# def create_app():
+#     app = Flask(__name__)
+#     app.config['SECRET_KEY'] = 'TEST'
+#     #database is stored at the location defined on the right
+#     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+#     db.init_app(app)
+    
+    
+#     from .views import views
+#     from .auth import auth
+
+#     app.register_blueprint(views, url_prefix='/')
+#     app.register_blueprint(auth, url_prefix='/')
+    
+#     return app
+
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -10,11 +31,14 @@ def create_app():
     #database is stored at the location defined on the right
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-    
+
+        
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    socketio = SocketIO(app)
     
-    return app
+    return socketio, app
