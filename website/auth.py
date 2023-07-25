18c36eee474f89bc721f1dfcfb24c7e69211222e
CSJLU <lucourage@gmail.com>
2023-07-25 16:19:08 -0400

    Reorganize code

--- website/auth.py
+++ website/auth.py
@@ -2,15 +2,19 @@ from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
+#ADD A ROUTE THAT APPEARS AFTER LOGIN FOR CHAT ROOM
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     #info inputted when accessing this route
     data = request.form
     return render_template("login.html")
 
+
 @auth.route('/logout')
 def logout():
     return "Logout"
+
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
