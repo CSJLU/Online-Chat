a27986a3cd92f49e44d26e0e6767cc75893a6746
CSJLU <lucourage@gmail.com>
2023-07-25 16:17:41 -0400

    Comment out

--- website/__init__.py
+++ website/__init__.py
@@ -20,25 +20,3 @@ from flask_socketio import join_room, leave_room, send, SocketIO
 #     app.register_blueprint(auth, url_prefix='/')
     
 #     return app
-
-
-db = SQLAlchemy()
-DB_NAME = "database.db"
-
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'TEST'
-    #database is stored at the location defined on the right
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
-
-        
-    from .views import views
-    from .auth import auth
-
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
-
-    socketio = SocketIO(app)
-    
-    return socketio, app
